ae42f4d43ac7007043f03da02e2a725eb869953d
herrnuessli <70325787+herrnuessli@users.noreply.github.com>
2022-06-30 17:08:14 +0200

    Add files via upload

---
+++ src/main/java/ch/bbw/personenverwaltung/PersonenverwaltungApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.personenverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonenverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonenverwaltungApplication.class, args);
+	}
+
+}
